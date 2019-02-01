d85835edeefd248ee47771213cbc638ad4ff589c
Jarl Patrick Paide <jarlpatrick@gmail.com>
2019-02-01 14:33:23 +0000

    lisatud ventilaatori kontrollimine

--- tracker.py
+++ tracker.py
@@ -1,10 +1,23 @@
 from pytrack.tracker import *
 from time import sleep
 import bme280
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+
+p = GPIO.PWM(25,50)
+p.start(7.5)
+
+def openfan():
+	p.ChangeDutyCycle(2.5)
+
+def closefan():
+	p.ChangeDutyCycle(7.5)
 
 def extra_telemetry():
 	tmp,pre,hum = bme280.readBME280All()
-	return "{:.2f}".format(tmp) + ',' + "{:.1f}".format(pre) + ',' + "{:.0f}".format(hum)
+	return "{:.2f}".format(tmp) + ',' + "{:.5f}".format(pre) + ',' + "{:.5f}".format(hum)
 
 MT = Tracker()
 
@@ -15,4 +28,7 @@ MT.start()
 MT.set_sentence_callback(extra_telemetry)
 
 while True:
-	sleep(1)
+	sleep(5)
+	openfan()
+	sleep(5)
+	closefan()
